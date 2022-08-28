4bf23ad4dd1e89c282ada66e5cca7a3c80434886
kamelion <elmalkhaseydina@gmail.com>
2022-08-29 01:43:11 +0200

    first commit

---
+++ ServerGestionStock/src/main/java/com/projet/ServerGestionStock/ServerGestionStockApplication.java
@@ -0,0 +1,13 @@
+package com.projet.ServerGestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockApplication.class, args);
+	}
+
+}
---
+++ ServerGestionStock/src/test/java/com/projet/ServerGestionStock/ServerGestionStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.ServerGestionStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
